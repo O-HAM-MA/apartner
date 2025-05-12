58de7bf31192990b94980c180fd2cae484a45721
JAWSP <lightload2912@gmail.com>
2025-05-12 16:49:15 +0900

    tmp
    # Conflicts:
    #	backend/src/main/java/com/ohammer/apartner/domain/inspection/controller/InspectionController.java
    #	backend/src/main/java/com/ohammer/apartner/domain/inspection/dto/InspectionRequestDto.java
    #	backend/src/main/java/com/ohammer/apartner/domain/inspection/dto/InspectionUpdateDto.java
    #	backend/src/main/java/com/ohammer/apartner/domain/inspection/service/InspectionService.java

--- backend/src/main/java/com/ohammer/apartner/domain/inspection/dto/InspectionRequestDto.java
+++ backend/src/main/java/com/ohammer/apartner/domain/inspection/dto/InspectionRequestDto.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDateTime;
