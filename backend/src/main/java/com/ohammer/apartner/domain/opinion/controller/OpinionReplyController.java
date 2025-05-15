516b6be2286586ccafa166cb685f4162272b60f2
jerry6475 <jerry6475@naver.com>
2025-05-15 13:17:18 +0900

    Feat: #30 Opinion(Manager)-CRUD

---
+++ backend/src/main/java/com/ohammer/apartner/domain/opinion/controller/OpinionReplyController.java
@@ -0,0 +1,11 @@
+package com.ohammer.apartner.domain.opinion.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/opinion/reply")
+public class OpinionReplyController {
+
+
+}
