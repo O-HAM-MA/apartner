ae880461698b75f835833786d2ca6b1230776421
bagsunyoung <malang333@naver.com.github.com>
2025-05-20 11:23:47 +0900

    Feat: #53 스웨거 태그로 api 설명 추가

--- backend/src/main/java/com/ohammer/apartner/domain/vehicle/controller/EntryRecordController.java
+++ backend/src/main/java/com/ohammer/apartner/domain/vehicle/controller/EntryRecordController.java
@@ -5,6 +5,7 @@ import com.ohammer.apartner.domain.vehicle.dto.EntryRecordResponseDto;
 import com.ohammer.apartner.domain.vehicle.dto.EntryRecordStatusDto;
 import com.ohammer.apartner.domain.vehicle.entity.EntryRecord;
 import com.ohammer.apartner.domain.vehicle.service.EntryRecordService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +19,7 @@ public class EntryRecordController {
 
     private final EntryRecordService entryRecordService;
 
+    @Tag(name = "주차 요청 승인/미승인 결정하는 동작")
     @PatchMapping("/{entryRecordId}/status")
     public ResponseEntity<EntryRecordStatusDto> updateEntryStatus(
             @PathVariable(value = "entryRecordId") Long entryRecordId,
@@ -29,6 +31,7 @@ public class EntryRecordController {
 
 
     // 🚗 입차
+    @Tag(name = "차 몰고 주차장으로 들어가는 동작")
     @PostMapping("/enter")
     public ResponseEntity<EntryRecordResponseDto> enter(@RequestBody EntryRecordRequestDto dto) {
 
@@ -36,18 +39,21 @@ public class EntryRecordController {
     }
 
     // 🚙 출차
+    @Tag(name = "주차장에서 차 빼서 나가는 동작")
     @PostMapping("/exit")
     public ResponseEntity<EntryRecordResponseDto> exit(@RequestBody EntryRecordRequestDto dto) { // @RequestBody EntryRecordRequestDto dto
         return ResponseEntity.ok(entryRecordService.exitVehicle(dto));
     }
 
     // 📜 출입 기록 전체 조회
+    @Tag(name = "차량의 출입 기록들 최신순 조회")
     @GetMapping("/{vehicleId}")
     public ResponseEntity<List<EntryRecordResponseDto>> getRecords(@PathVariable(value = "vehicleId") Long vehicleId) {
         return ResponseEntity.ok(entryRecordService.getEntryRecords(vehicleId));
     }
 
     // 🚗 차량이 다시 주차 허가를 요청할 때 (출입 신청)
+    @Tag(name = "이미 등록한 차량이 다시 주차장에 들어가려 등록하는 동작")
     @PostMapping("/request/{vehicleId}")
     public ResponseEntity<EntryRecordResponseDto> requestEntryRecord() { // @PathVariable(value = "vehicleId") Long vehicleId
         EntryRecordResponseDto response = entryRecordService.requestEntryRecord();
@@ -55,13 +61,13 @@ public class EntryRecordController {
     }
 
     // ✅ 가장 최근 PENDING 상태의 출입기록 상태 변경 (ex. AGREE, REJECT)
-    @PutMapping("/update-status/{vehicleId}")
-    public ResponseEntity<EntryRecordStatusDto> updateLatestPendingStatus(
-            @PathVariable(value = "vehicleId") Long vehicleId,
-            @RequestParam EntryRecord.Status status) {
-
-        EntryRecordStatusDto updated = entryRecordService.updateLatestPendingStatus(vehicleId, status);
-        return ResponseEntity.ok(updated);
-    }
+//    @PutMapping("/update-status/{vehicleId}")
+//    public ResponseEntity<EntryRecordStatusDto> updateLatestPendingStatus(
+//            @PathVariable(value = "vehicleId") Long vehicleId,
+//            @RequestParam EntryRecord.Status status) {
+//
+//        EntryRecordStatusDto updated = entryRecordService.updateLatestPendingStatus(vehicleId, status);
+//        return ResponseEntity.ok(updated);
+//    }
 
 }
--- backend/src/main/java/com/ohammer/apartner/domain/vehicle/controller/VehicleController.java
+++ backend/src/main/java/com/ohammer/apartner/domain/vehicle/controller/VehicleController.java
@@ -4,6 +4,7 @@ package com.ohammer.apartner.domain.vehicle.controller;
 import com.ohammer.apartner.domain.vehicle.dto.*;
 import com.ohammer.apartner.domain.vehicle.service.VehicleService;
 import com.ohammer.apartner.security.utils.SecurityUtil;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +20,7 @@ public class VehicleController {
     private final VehicleService vehicleService;
 
     // 입주민 차량 등록
+    @Tag(name = "입주민이 주차장에 주차하러 차량 등록")
     @PostMapping("/residents")
     public ResponseEntity<VehicleResponseDto> registerResidentVehicle(@RequestBody ResidentVehicleRequestDto dto) {
         VehicleResponseDto response = vehicleService.registerResidentVehicle(dto);
@@ -26,6 +28,7 @@ public class VehicleController {
     }
 
     // 외부 차량 등록
+    @Tag(name = "외부 손님이 주차장에 주차하러 차량 등록")
     @PostMapping("/foreigns")
     public ResponseEntity<VehicleResponseDto> registerForeignVehicle(@RequestBody ForeignVehicleRequestDto dto) {
         VehicleResponseDto response = vehicleService.registerForeignVehicle(dto);
@@ -33,23 +36,24 @@ public class VehicleController {
     }
 
 
-    @GetMapping("/resident")
-    public ResponseEntity<List<VehicleResponseDto>> getResidentVehicles() {
-        return ResponseEntity.ok(vehicleService.getResidentVehicles());
-    }
+//    @GetMapping("/resident")
+//    public ResponseEntity<List<VehicleResponseDto>> getResidentVehicles() {
+//        return ResponseEntity.ok(vehicleService.getResidentVehicles());
+//    }
+//
+//    @GetMapping("/foreign")
+//    public ResponseEntity<List<VehicleResponseDto>> getForeignVehicles() {
+//        return ResponseEntity.ok(vehicleService.getForeignVehicles());
+//    }
 
-    @GetMapping("/foreign")
-    public ResponseEntity<List<VehicleResponseDto>> getForeignVehicles() {
-        return ResponseEntity.ok(vehicleService.getForeignVehicles());
-    }
+//    @GetMapping("/registrations")
+//    public ResponseEntity<List<VehicleRegistrationInfoDto>> getVehicleRegistrations(
+//            @RequestParam(name = "isForeign",  required = false) Boolean isForeign) {
+//        List<VehicleRegistrationInfoDto> registrations = vehicleService.getVehicleRegistrationInfo(isForeign);
+//        return ResponseEntity.ok(registrations);
+//    }
 
-    @GetMapping("/registrations")
-    public ResponseEntity<List<VehicleRegistrationInfoDto>> getVehicleRegistrations(
-            @RequestParam(name = "isForeign",  required = false) Boolean isForeign) {
-        List<VehicleRegistrationInfoDto> registrations = vehicleService.getVehicleRegistrationInfo(isForeign);
-        return ResponseEntity.ok(registrations);
-    }
-
+    @Tag(name = "등록된 모든 차량 조회")
     @GetMapping("/registrationsWithStatus")
     public ResponseEntity<List<VehicleRegistrationInfoDto>> getRegistrations(
             @RequestParam(value = "isForeign", required = false) Boolean isForeign
@@ -59,13 +63,14 @@ public class VehicleController {
     }
 
 
-
+    @Tag(name = "등록된 차량을 수정")
     @PatchMapping("/update/{vehicleId}")
     public void updateVehicle(@PathVariable(value = "vehicleId") Long vehicleId,
                               @RequestBody VehicleUpdateRequestDto dto) {
         vehicleService.updateVehicle(vehicleId, dto);
     }
 
+    @Tag(name = "등록된 차량을 삭제")
     @DeleteMapping("/delete/{vehicleId}")
     public ResponseEntity<String> deleteVehicle(@PathVariable(value = "vehicleId") Long vehicleId) {
         try {
@@ -77,12 +82,14 @@ public class VehicleController {
     }
 
 
+    @Tag(name = "주차 권한을 가진 차량들만 조회")
     @GetMapping("/approved")
     public ResponseEntity<List<VehicleRegistrationInfoDto>> getApprovedVehicles() {
         List<VehicleRegistrationInfoDto> approvedVehicles = vehicleService.getApprovedVehicles();
         return ResponseEntity.ok(approvedVehicles);
     }
 
+    @Tag(name = "주차 승인을 받지 못해 대기하고 있는 외부 차량 리스트 조회")
     @GetMapping("/visitors/pending")
     public ResponseEntity<List<VehicleRegistrationInfoDto>> getMyRequests() {
         Long inviterId = SecurityUtil.getCurrentUserId();
@@ -95,23 +102,27 @@ public class VehicleController {
         );
     }
 
+    @Tag(name = "입주민의 허가는 받았지만 관리자의 최종 승인은 받지 못해 대기하는 외부 차량 리스트 조회")
     @GetMapping("/invited-approved")
     public ResponseEntity<List<VehicleRegistrationInfoDto>> getInvitedApprovedVehicles() {
         List<VehicleRegistrationInfoDto> approvedVehicles = vehicleService.getInvitedApprovedVehicles();
         return ResponseEntity.ok(approvedVehicles);
     }
 
+    @Tag(name = "현재 주차 중인 차량 리스트 조회")
     @GetMapping("/active")
     public ResponseEntity<List<VehicleRegistrationInfoDto>> getActiveVehicles() {
         List<VehicleRegistrationInfoDto> list = vehicleService.getActiveVehicles();
         return ResponseEntity.ok(list);
     }
 
+    @Tag(name = "주차랑 수용 공간과 현재 주차된 차량 수, 그리고 남은 주차 공간")
     @GetMapping("/status")
     public ResponseEntity<ParkingStatusDto> getParkingStatus() {
         return ResponseEntity.ok(vehicleService.getParkingStatus());
     }
 
+    @Tag(name = "입주민이 자신 앞으로 등록된 차량 리스트 조회")
     @GetMapping("/mine")
     public ResponseEntity<List<VehicleRegistrationInfoDto>> getMyVehicles() {
         List<VehicleRegistrationInfoDto> list = vehicleService.getMyVehicleRegistrations();
