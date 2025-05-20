cdf91ba36eb4b2ac58d0ad6b4cdafa75e55a5bd4
jerry6475 <jerry6475@naver.com>
2025-05-20 13:18:05 +0900

    Refact: #54 url, todayCount

--- backend/src/main/java/com/ohammer/apartner/domain/complaint/controller/ComplaintStatisticsController.java
+++ backend/src/main/java/com/ohammer/apartner/domain/complaint/controller/ComplaintStatisticsController.java
@@ -2,9 +2,9 @@ package com.ohammer.apartner.domain.complaint.controller;
 
 import com.ohammer.apartner.domain.complaint.dto.response.ComplaintCountByStatusResponseDto;
 import com.ohammer.apartner.domain.complaint.dto.response.ComplaintHandlingRateResponseDto;
-import com.ohammer.apartner.domain.complaint.dto.response.TodayComplaintResponseDto;
 import com.ohammer.apartner.domain.complaint.service.ComplaintStatisticsService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +16,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/complaints/statistics")
 @RequiredArgsConstructor
+@Tag(name = "민원 통계 API")
 public class ComplaintStatisticsController {
 
     private final ComplaintStatisticsService complaintStatisticsService;
@@ -24,7 +25,7 @@ public class ComplaintStatisticsController {
     @Operation(summary = "일별 민원 수 조회", description = "일별 민원 수, 상태 조회")
     public ResponseEntity<?> getTodayStats() throws AccessDeniedException {
 
-        List<TodayComplaintResponseDto> response = complaintStatisticsService.getTodayComplaintStats();
+        Long response = complaintStatisticsService.getTodayTotalComplaintCount();
 
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
