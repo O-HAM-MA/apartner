263ba4df02bf54670260bff395c38667bce6d6ec
JAWSP <lightload2912@gmail.com>
2025-05-30 17:41:44 +0900

    tmp

--- backend/src/main/java/com/ohammer/apartner/ApartnerApplication.java
+++ backend/src/main/java/com/ohammer/apartner/ApartnerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+//a
 public class ApartnerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApartnerApplication.class, args);
